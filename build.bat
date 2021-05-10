dcdba49d364e7fb8d9c5e0eb243ea53febf9b419
mim <mim@hotmail.co.uk>
2021-05-10 07:24:54 +0100

    Enable optimisations, disable RTTI and exceptions

--- build.bat
+++ build.bat
@@ -1,7 +1,7 @@
 @echo off
 if exist hex.obj del hex.obj
 if exist hex.exe del hex.exe
-cl /nologo /MD hex.cpp ^
+cl /nologo /MD /O2 /GR- /EHa- hex.cpp ^
    /I raylib/include ^
    /link /SUBSYSTEM:WINDOWS /entry:mainCRTStartup /LTCG ^
    raylib/lib/raylib.lib Winmm.lib User32.lib Gdi32.lib Shell32.lib
